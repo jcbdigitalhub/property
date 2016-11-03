4f58bed1cd0d7f96dff13cc64dc5504171eaaf14
root <jeff@ossph.com>
2016-11-03 08:12:34 +0000

    modified:   property/property/doctype/charge/charge.json 	modified:   property/property/doctype/other_charges/other_charges.js 	modified:   property/property/doctype/other_charges/other_charges.json 	modified:   property/property/doctype/other_charges_items/other_charges_items.json 	modified:   property/property/doctype/unit/unit.json

--- property/property/doctype/other_charges/other_charges.js
+++ property/property/doctype/other_charges/other_charges.js
@@ -4,5 +4,33 @@
 frappe.ui.form.on('Other Charges', {
 	refresh: function(frm) {
 
-	}
+	},
+
+        setup: function(frm) {
+                frm.fields_dict['other_charges_items'].grid.get_field('charge').get_query = function(frm, cdt, cdn) {
+                        return{
+                                filters: {
+                                        'other_charge': 1
+                                }
+                        }
+                }
+       }
+
+
+});
+
+frappe.ui.form.on('Other Charges Items', {
+	refresh: function(frm) {},
+
+	rate: function(frm,cdt,cdn) {
+                        d = locals[cdt][cdn];
+                        r = d.rate * d.quantity;
+                        frappe.model.set_value(cdt, cdn, "amount", r);
+	},
+
+        quantity: function(frm,cdt,cdn) {
+                        d = locals[cdt][cdn];
+                        r = d.rate * d.quantity;
+                        frappe.model.set_value(cdt, cdn, "amount", r);
+        }
 });
