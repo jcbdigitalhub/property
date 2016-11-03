f2e1dcf6ca194392754d51a74f1a385cf895dd34
root <jeff@ossph.com>
2016-11-03 03:48:27 +0000

    modified:   property/property/doctype/charge/charge.json 	modified:   property/property/doctype/meter_reading/meter_reading.js 	modified:   property/property/doctype/meter_reading/meter_reading.json 	modified:   property/property/doctype/meter_reading/meter_reading.py 	modified:   property/property/doctype/unit_charge/unit_charge.json 	modified:   property/property/doctype/unit_meter_reading/unit_meter_reading.json

--- property/property/doctype/meter_reading/meter_reading.js
+++ property/property/doctype/meter_reading/meter_reading.js
@@ -4,5 +4,29 @@
 frappe.ui.form.on('Meter Reading', {
 	refresh: function(frm) {
 
+	},
+
+	generate_submeters: function(frm){
+		cur_frm.call('get_submeters','', function(r) {
+			if(r.message) {
+				$.each(r.message, function(i,d) {
+					var row = frappe.model.add_child(frm.doc, "Unit Meter Reading", "unit_reading");
+					row.unit = d.unit;
+					row.previous_reading = d.previous_reading;
+				});
+			}
+			refresh_field("unit_reading");
+		});
 	}
+});
+
+frappe.ui.form.on('Unit Meter Reading', {
+        refresh: function(frm) {},
+
+	current_reading: function(frm,cdt,cdn) {
+		var d = locals[cdt][cdn];
+		var u = d.current_reading - d.previous_reading;
+		frappe.model.set_value(cdt,cdn, "usage", u);
+	}
+
 });
--- property/property/doctype/meter_reading/meter_reading.py
+++ property/property/doctype/meter_reading/meter_reading.py
@@ -5,6 +5,15 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import msgprint, throw, _
+
 
 class MeterReading(Document):
 	pass
+
+	def get_submeters(self):
+		r = frappe.db.sql("""SELECT u.name as unit, b.previous_reading as previous_reading """+
+			"""FROM `tabUnit Charge` b, tabUnit u, tabCharge c, `tabMain Meter` m"""+
+			" WHERE u.name = b.parent AND b.charge = c.name AND c.charge_type = 'Meter' AND b.main_meter = m.name AND m.name = %s",self.main_meter, as_dict = 1)
+		return r
+ 
