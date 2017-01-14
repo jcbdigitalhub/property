6257cfe650ea39132a0c33968c19775f033a3dbb
root <jeff@ossph.com>
2017-01-14 00:43:58 +0000

    modified:   property/config/docs.py 	new file:   property/docs/__init__.py 	new file:   property/docs/assets/__init__.py 	new file:   property/docs/assets/img/__init__.py 	new file:   property/docs/contents.html 	new file:   property/docs/contents.py 	new file:   property/docs/index.html 	new file:   property/docs/index.txt 	new file:   property/docs/install.md 	new file:   property/docs/license.html 	new file:   property/docs/user/__init__.py

--- property/config/docs.py
+++ property/config/docs.py
@@ -1,11 +1,18 @@
-"""
-Configuration for docs
-"""
-
-# source_link = "https://github.com/[org_name]/property"
-# docs_base_url = "https://[org_name].github.io/property"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+source_link = "https://github.com/ossphcorp/property"
+docs_base_url = "https://ossphcorp.github.io/property"
+headline = "PropertyNext"
+sub_heading = "Property Management App for ERPNext"
+long_description = """(long description in markdown)"""
 
 def get_context(context):
-	context.brand_html = "Property"
+    # optional settings
+
+    # context.brand_html = 'Brand info on the top left'
+    # context.favicon = 'path to favicon'
+    #
+    context.top_bar_items = [
+      {"label": "About", "url": context.docs_base_url + "/about"},
+    ]
+
+pass
+
